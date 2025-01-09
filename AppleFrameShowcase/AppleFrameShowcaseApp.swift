1400b303864eaddb2f00158f71d988bd179ebe76
Chukwubuikem Chukwudi <chuk.developa@gmail.com>
2025-01-09 21:06:57 +0100

    Initial Commit

---
+++ AppleFrameShowcase/AppleFrameShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameShowcaseApp.swift
+//  AppleFrameShowcase
+//
+//  Created by Eco Dev S-SSD  on 09/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleFrameShowcase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleFrameShowcase
+//
+//  Created by Eco Dev S-SSD  on 09/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
